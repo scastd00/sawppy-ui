9b1015e18f08ae008da7b74181cfedf2071b0140
scastd00 <scastd00@estudiantes.unileon.es>
2022-11-23 00:16:20 +0100

    Helper methods in Action class

--- app/src/main/java/es/unileon/sawppy/ui/Action.kt
+++ app/src/main/java/es/unileon/sawppy/ui/Action.kt
@@ -4,13 +4,13 @@ package es.unileon.sawppy.ui
  * Data class that represents the different actions that can be performed by the user.
  *
  * @param signalToSend The signal to send to the rover. Numbering is done as follows:
- * - 10x: movement commands
- * - 20x: control mode commands
- * - 30x: camera commands
- * - 40x: light commands
- * - 50x: sound commands
- * - 60x: sensor commands
- * - 70x: other commands
+ * - 1xx: movement commands
+ * - 2xx: control mode commands
+ * - 3xx: camera commands
+ * - 4xx: light commands
+ * - 5xx: sound commands
+ * - 6xx: sensor commands
+ * - 7xx: other commands
  */
 data class Action(val signalToSend: Int) {
 	companion object {
@@ -23,24 +23,57 @@ data class Action(val signalToSend: Int) {
 		val MANUAL = Action(207)
 	}
 
+	private fun getTypeNumber(): Int {
+		return this.signalToSend / 100
+	}
+
+	private fun getNumber(): Int {
+		return this.signalToSend % 100
+	}
+
+	fun isMovement(): Boolean {
+		return this.getTypeNumber() == 1
+	}
+
+	fun isControlMode(): Boolean {
+		return this.getTypeNumber() == 2
+	}
+
+	fun isCamera(): Boolean {
+		return this.getTypeNumber() == 3
+	}
+
+	fun isLight(): Boolean {
+		return this.getTypeNumber() == 4
+	}
+
+	fun isSound(): Boolean {
+		return this.getTypeNumber() == 5
+	}
+
+	fun isSensor(): Boolean {
+		return this.getTypeNumber() == 6
+	}
+
+	fun isOther(): Boolean {
+		return this.getTypeNumber() == 7
+	}
+
 	/**
 	 * @return String with the content to send to the rover.
 	 */
 	override fun toString(): String {
-		val signalString = this.signalToSend.toString()
-		val actionType = when (signalString[0]) {
-			'1' -> "movement"
-			'2' -> "control"
-			'3' -> "camera"
-			'4' -> "light"
-			'5' -> "sound"
-			'6' -> "sensor"
-			'7' -> "other"
+		val actionType = when (getTypeNumber()) {
+			1 -> "movement"
+			2 -> "control"
+			3 -> "camera"
+			4 -> "light"
+			5 -> "sound"
+			6 -> "sensor"
+			7 -> "other"
 			else -> throw IllegalArgumentException("Invalid action type")
 		}
 
-		val signalNumberToSend = signalString.substring(1).toInt()
-
-		return """{"$actionType": "$signalNumberToSend"}"""
+		return """{"$actionType": ${getNumber()}}"""
 	}
 }
