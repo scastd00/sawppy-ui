6011ba3e023e92869e799b404ba1002828f6236b
scastd00 <scastd00@estudiantes.unileon.es>
2022-11-23 19:13:14 +0100

    Removed todo comments

--- app/src/main/java/es/unileon/sawppy/ui/MainActivity.kt
+++ app/src/main/java/es/unileon/sawppy/ui/MainActivity.kt
@@ -68,13 +68,6 @@ class MainActivity : AppCompatActivity() {
 				Manifest.permission.BLUETOOTH_CONNECT
 			) == PackageManager.PERMISSION_DENIED
 		) {
-			// TODO: Consider calling
-			//    ActivityCompat#requestPermissions
-			// here to request the missing permissions, and then overriding
-			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-			//                                          int[] grantResults)
-			// to handle the case where the user grants the permission. See the documentation
-			// for ActivityCompat#requestPermissions for more details.
 			return
 		} else {
 			this.bluetoothAdapter.bondedDevices
@@ -241,13 +234,6 @@ class MainActivity : AppCompatActivity() {
 					Manifest.permission.BLUETOOTH_CONNECT
 				) == PackageManager.PERMISSION_DENIED
 			) {
-				// TODO: Consider calling
-				//    ActivityCompat#requestPermissions
-				// here to request the missing permissions, and then overriding
-				//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-				//                                          int[] grantResults)
-				// to handle the case where the user grants the permission. See the documentation
-				// for ActivityCompat#requestPermissions for more details.
 				ActivityCompat.requestPermissions(
 					this@MainActivity,
 					arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
@@ -278,13 +264,6 @@ class MainActivity : AppCompatActivity() {
 					Manifest.permission.BLUETOOTH_SCAN
 				) == PackageManager.PERMISSION_DENIED
 			) {
-				// TODO: Consider calling
-				//    ActivityCompat#requestPermissions
-				// here to request the missing permissions, and then overriding
-				//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-				//                                          int[] grantResults)
-				// to handle the case where the user grants the permission. See the documentation
-				// for ActivityCompat#requestPermissions for more details.
 				return
 			}
 			bluetoothAdapter.cancelDiscovery()
